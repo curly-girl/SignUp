6be46678f13e095adad03b7207e973c085bcbd90
Ekaterina Presnetsova <kate8607@gmail.com>
2015-03-22 13:44:10 -0700

    Added additional test cases

--- src/main/java/core/Selenium_SignUp/Selenium_SignUp.java
+++ src/main/java/core/Selenium_SignUp/Selenium_SignUp.java
@@ -20,12 +20,15 @@ public class Selenium_SignUp {
 		String text_case_id_06 = "TC-001.06";
 		String text_case_id_07 = "TC-001.07";
 		String text_case_id_08 = "TC-001.08";
+		String text_case_id_09 = "TC-001.09";
 		String url = "http://learn2test.net/qa/apps/sign_up/v1/";
-		String title_sign_up_expected = "Welcome to Sign Up";
+		String title_sign_up_expected = "Welcome to Sign Up v1";
 		String title_facebook_expected = "Welcome to Facebook - Log In, Sign Up or Learn More";
 		String title_twitter_expected = "Welcome to Twitter - Login or Sign up";
+		String title_flicker_expected = "Welcome to Flickr - Photo Sharing";
+		String title_youtube_expected = "YouTube";
 		String error_fname_empty_expected = "Please enter First Name";
-		String error_lname_empty_expected ="Please enter Last Name";
+		String error_lname_empty_expected = "Please enter Last Name";
 		String fname = "Alex";
 		String lname = "Moore";
 		String email = "alexmoore@gmail.com";
@@ -75,46 +78,137 @@ public class Selenium_SignUp {
 					+ title_facebook_expected + "/" + title_facebook_actual);
 			System.out.println("=======================================");
 		}
-
 		// TC-001.03
+		driver.findElement(By.id("id_img_twitter")).click();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		allTabs = new ArrayList<String>(driver.getWindowHandles());
+		// String tab = driver.getWindowHandle();
+		driver.switchTo().window(allTabs.get(1));
+		String title_twitter_actual = driver.getTitle();
+		driver.close();
+		driver.switchTo().window(allTabs.get(0));
+
+		if (title_twitter_expected.equals(title_twitter_actual)) {
+			System.out.println("Test case ID: \t\t" + text_case_id_03
+					+ "-PASSED");
+			System.out.println("Title Expected/Actual: \t"
+					+ title_twitter_expected + "/" + title_twitter_actual);
+			System.out.println("===========================================");
+		} else {
+			System.out.println("Test Case ID: \t\t" + text_case_id_03
+					+ " - FAILED");
+			System.out.println("Title Expected/Actual: \t"
+					+ title_twitter_expected + "/" + title_twitter_actual);
+			System.out.println("=======================================");
+		}
+		// TC-001.04
+		driver.findElement(
+				By.xpath("/html/body/form/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[6]/td[2]/a[3]/img"))
+				.click();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		allTabs = new ArrayList<String>(driver.getWindowHandles());
+		// String tab = driver.getWindowHandle();
+		driver.switchTo().window(allTabs.get(1));
+		String title_flicker_actual = driver.getTitle();
+		driver.close();
+		driver.switchTo().window(allTabs.get(0));
+
+		if (title_flicker_expected.equals(title_flicker_actual)) {
+			System.out.println("Test case ID: \t\t" + text_case_id_04
+					+ "-PASSED");
+			System.out.println("Title Expected/Actual: \t"
+					+ title_flicker_expected + "/" + title_flicker_actual);
+			System.out.println("===========================================");
+		} else {
+			System.out.println("Test Case ID: \t\t" + text_case_id_04
+					+ " - FAILED");
+			System.out.println("Title Expected/Actual: \t"
+					+ title_flicker_expected + "/" + title_flicker_actual);
+			System.out.println("=======================================");
+		}
+		// TC-001.05
+		driver.findElement(By.id("id_img_youtube")).click();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		ArrayList<String> allTabs1 = new ArrayList<String>(
+				driver.getWindowHandles());
+		driver.switchTo().window(allTabs1.get(1));
+		String title_youtube_actual = driver.getTitle();
+		driver.close();
+		driver.switchTo().window(allTabs1.get(0));
+
+		if (title_youtube_expected.equals(title_youtube_actual)) {
+			System.out.println("Test Case ID: \t\t" + text_case_id_05
+					+ " - PASSED");
+			System.out.println("Title Expected/Actual: \t"
+					+ title_youtube_expected + "/" + title_youtube_actual);
+			System.out
+					.println("===============================================");
+		} else {
+			System.out.println("Test Case ID: \t\t" + text_case_id_05
+					+ " - FAILED");
+			System.out.println("Title Expected/Actual: \t"
+					+ title_youtube_expected + "/" + title_youtube_actual);
+			System.out
+					.println("===============================================");
+		}
+
+		// TC-001.06
 
 		driver.findElement(By.id("id_quotes")).getText();
 		String quote = driver.findElement(By.id("id_quotes")).getText();
 		if (quote != null && quote.length() < 103 && quote.length() > 36) {
-			System.out.println("Test Case ID: \t\t" + text_case_id_03
+			System.out.println("Test Case ID: \t\t" + text_case_id_06
 					+ " - PASSED");
 			System.out.println("Quote length: " + quote.length());
 			System.out.println("=======================================");
 		} else {
-			System.out.println("Test Case ID: \t\t" + text_case_id_03
+			System.out.println("Test Case ID: \t\t" + text_case_id_06
 					+ " - FAILED");
 			System.out.println("Quote length: " + quote.length());
 			System.out.println("=======================================");
 		}
 
-		// TC-001.04
+		// TC-001.07
 
 		driver.findElement(By.id("id_submit_button")).click();
-		String error_fname_empty_actual = driver
-				.findElement(By.id("ErrorLine")).getText();
+		String error_fname_empty_actual = driver.findElement(By.id("ErrorLine")).getText();
 
 		if (error_fname_empty_expected.equals(error_fname_empty_actual)) {
-			System.out.println("Test Case ID: \t\t" + text_case_id_04
+			System.out.println("Test Case ID: \t\t" + text_case_id_07
 					+ " - PASSED");
 			System.out.println("Error Expected/Actual: \t"
 					+ error_fname_empty_expected + "/"
 					+ error_fname_empty_actual);
 			System.out.println("=======================================");
 		} else {
-			System.out.println("Test Case ID: \t\t" + text_case_id_04
+			System.out.println("Test Case ID: \t\t" + text_case_id_07
 					+ " - FAILED");
 			System.out.println("Error Expected/Actual: \t"
 					+ error_fname_empty_expected + "/"
 					+ error_fname_empty_actual);
 			System.out.println("=======================================");
 		}
+		
+		//TC-001.08
+		driver.findElement(By.id("id_fname")).sendKeys(fname);
+		driver.findElement(By.id("id_submit_button")).click();
+		String error_lname_empty_actual = driver.findElement(By.id("ErrorLine")).getText();
+		
+		if(error_lname_empty_expected.equals(error_lname_empty_actual)){
+			System.out.println("Test Case ID: \t\t" + text_case_id_08 + "- PASSED");
+			System.out.println("Error Expected/Actul: \t" + error_lname_empty_expected + "/" + error_lname_empty_actual);
+			System.out.println("======================================");	
+		} else {
+			System.out.println("Test Case ID: \t\t" + text_case_id_08
+					+ " - FAILED");
+			System.out.println("Error Expected/Actual: \t"
+					+ error_lname_empty_expected + "/"
+					+ error_lname_empty_actual);
+			System.out.println("=======================================");
+			
+		}
 
-		// TC-001.05
+		// TC-001.09
 
 		driver.findElement(By.id("id_fname")).sendKeys(fname);
 		driver.findElement(By.id("id_lname")).sendKeys(lname);
@@ -134,7 +228,7 @@ public class Selenium_SignUp {
 		if (fname.equals(fname_conf_actual) && lname.equals(lname_conf_actual)
 				&& email.equals(email_conf_actual)
 				&& phone.equals(phone_conf_actual)) {
-			System.out.println("Test Case ID: \t\t" + text_case_id_05
+			System.out.println("Test Case ID: \t\t" + text_case_id_09
 					+ " - PASSED");
 			System.out.println("First Expected/Actual: \t" + fname + "/"
 					+ fname_conf_actual);
@@ -146,7 +240,7 @@ public class Selenium_SignUp {
 					+ phone_conf_actual);
 			System.out.println("=======================================");
 		} else {
-			System.out.println("Test Case ID: \t\t" + text_case_id_05
+			System.out.println("Test Case ID: \t\t" + text_case_id_09
 					+ " - FAILED");
 			System.out.println("First Expected/Actual: \t" + fname + "/"
 					+ fname_conf_actual);
@@ -158,53 +252,8 @@ public class Selenium_SignUp {
 					+ phone_conf_actual);
 			System.out.println("=======================================");
 		}
-		//TC-001.06
-		driver.get(url);
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.findElement(By.id("id_img_twitter")).click();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		allTabs = new ArrayList<String>(driver.getWindowHandles());
-		//String tab = driver.getWindowHandle();
-		driver.switchTo().window(allTabs.get(1));
-		String title_twitter_actual = driver.getTitle();
-		driver.close();
-		driver.switchTo().window(allTabs.get(0));
-		
-		if(title_twitter_expected.equals(title_twitter_actual)) {
-			System.out.println("Test case ID: \t\t" + text_case_id_06 + "-PASSED" );
-			System.out.println("Title Expected/Actual: \t"+ title_twitter_expected + "/"+ title_twitter_actual);
-			System.out.println("===========================================");
-		}else{
-			System.out.println("Test Case ID: \t\t" + text_case_id_06
-					+ " - FAILED");
-			System.out.println("Title Expected/Actual: \t"
-					+ title_twitter_expected + "/" + title_twitter_actual);
-			System.out.println("=======================================");
-		}
-		//TC-001.07
-		driver.findElement(By.id("id_fname")).sendKeys(fname);
-		driver.findElement(By.id("id_submit_button")).click();
-		String error_lname_empty_actual = driver
-				.findElement(By.id("ErrorLine")).getText();
-
-		if (error_lname_empty_expected.equals(error_lname_empty_actual)) {
-			System.out.println("Test Case ID: \t\t" + text_case_id_07
-					+ " - PASSED");
-			System.out.println("Error Expected/Actual: \t"
-					+ error_lname_empty_expected + "/"
-					+ error_lname_empty_actual);
-			System.out.println("=======================================");
-		} else {
-			System.out.println("Test Case ID: \t\t" + text_case_id_07
-					+ " - FAILED");
-			System.out.println("Error Expected/Actual: \t"
-					+ error_lname_empty_expected + "/"
-					+ error_lname_empty_actual);
-			System.out.println("=======================================");
-		}
-		
 
 		driver.quit();
 	}
 }
-//END
+// END
